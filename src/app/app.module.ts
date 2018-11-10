32e180880b4570a906b132dde2d280b80aae6334
Nicola Toledo <>
2018-11-10 10:50:28 +0100

    aggiunto le rotte al progetto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,12 @@ import { MDBBootstrapModule } from "angular-bootstrap-md";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { HomeComponent } from './home/home.component';
+import { InfoComponent } from './info/info.component';
+import { ContattiComponent } from './contatti/contatti.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, InfoComponent, ContattiComponent],
   imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
